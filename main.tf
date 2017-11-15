0a4a247516e1c75e74fc4f688f75ce839bbf5faf
Neil Haggerty <neil.haggerty@mergermarket.com>
2017-11-15 11:01:12 +0000

    Initial commit.
    JIRA: PLAT-1199

---
+++ main.tf
@@ -0,0 +1,47 @@
+resource "aws_alb_target_group" "target_group" {
+  name = "${replace(replace(var.name, "/(.{0,32}).*/", "$1"), "/^-+|-+$/", "")}"
+
+  # port will be set dynamically, but for some reason AWS requires a value
+  port                 = "31337"
+  protocol             = "HTTP"
+  vpc_id               = "${var.vpc_id}"
+  deregistration_delay = "${var.deregistration_delay}"
+
+  health_check {
+    interval            = "${var.health_check_interval}"
+    path                = "${var.health_check_path}"
+    timeout             = "${var.health_check_timeout}"
+    healthy_threshold   = "${var.health_check_healthy_threshold}"
+    unhealthy_threshold = "${var.health_check_unhealthy_threshold}"
+    matcher             = "${var.health_check_matcher}"
+  }
+
+  lifecycle {
+    create_before_destroy = true
+  }
+}
+
+resource "aws_alb_listener_rule" "rule" {
+  count = "${length(var.path_conditions)}"
+
+  listener_arn = "${var.alb_listener_arn}"
+  priority     = "${var.starting_priority + count.index}"
+
+  action {
+    type             = "forward"
+    target_group_arn = "${var.target_group_arn}"
+  }
+
+  # Always pass host-based routing condition, with '*.*' being default
+  #
+  # NOTE: You can have multiple paths but only a single hostname
+  condition {
+    field  = "host-header"
+    values = ["${var.host_condition}"]
+  }
+
+  condition {
+    field  = "path-pattern"
+    values = ["${element(var.path_conditions, count.index)}"]
+  }
+}
---
+++ variables.tf
@@ -0,0 +1,78 @@
+# required
+variable "name" {
+  description = "Name of the target group"
+  type        = "string"
+}
+
+variable "vpc_id" {
+  description = "The ID of the VPC to create the target group in"
+  type        = "string"
+}
+
+variable "alb_listener_arn" {
+  description = "Listener to add the rule(s) to"
+  type        = "string"
+}
+
+variable "target_group_arn" {
+  description = "Target group to direct traffic to"
+  type        = "string"
+}
+
+variable "path_conditions" {
+  description = "Defines path-based conditions for routing; separate by, eg. '/home,/home/*'"
+  type        = "list"
+}
+
+variable "starting_priority" {
+  description = "Starting priority for rules that will be added"
+}
+
+# optional
+variable "host_condition" {
+  description = "Defines host-based condition for rule (domain name)"
+  type        = "string"
+  default     = "*.*"
+}
+
+variable "deregistration_delay" {
+  description = "The amount time for Elastic Load Balancing to wait before changing the state of a deregistering target from draining to unused. The range is 0-3600 seconds."
+  type        = "string"
+  default     = "10"
+}
+
+variable "health_check_interval" {
+  description = "The approximate amount of time, in seconds, between health checks of an individual target. Minimum value 5 seconds, Maximum value 300 seconds."
+  type        = "string"
+  default     = "5"
+}
+
+variable "health_check_path" {
+  description = "The destination for the health check request."
+  type        = "string"
+  default     = "/internal/healthcheck"
+}
+
+variable "health_check_timeout" {
+  description = "The amount of time, in seconds, during which no response means a failed health check."
+  type        = "string"
+  default     = "4"
+}
+
+variable "health_check_healthy_threshold" {
+  description = "The number of consecutive health checks successes required before considering an unhealthy target healthy."
+  type        = "string"
+  default     = "2"
+}
+
+variable "health_check_unhealthy_threshold" {
+  description = "The number of consecutive health check failures required before considering the target unhealthy."
+  type        = "string"
+  default     = "2"
+}
+
+variable "health_check_matcher" {
+  description = "The HTTP codes to use when checking for a successful response from a target. You can specify multiple values (for example, \"200,202\") or a range of values (for example, \"200-299\")."
+  type        = "string"
+  default     = "200-299"
+}
